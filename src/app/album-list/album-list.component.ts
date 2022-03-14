587ded7d270cfc5044baf3c0c5c8178f5a6cfe8d
Alba <alba.m.navarro@gmail.com>
2022-03-14 15:24:32 +0100

    feat all albums

--- src/app/album-list/album-list.component.ts
+++ src/app/album-list/album-list.component.ts
@@ -1,18 +1,26 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+
+
 @Component({
   selector: 'album-list',
   templateUrl: './album-list.component.html',
   styleUrls: ['./album-list.component.scss']
 })
+
+
 export class AlbumListComponent implements OnInit {
   albumList: Array<any> = [];
+  
+  
 
   constructor( private http: HttpClient) { }
 
   ngOnInit(): void {
-    this.http.get<any[]>('http://localhost:3000/album').subscribe((data: Array<any>) =>
-      this.albumList = data
+    this.http.get<any[]>('http://localhost:3000/album').subscribe((data: Array<any>) =>{
+      this.albumList = data;
+      console.log(data)
+  }
     );
   }
 
@@ -20,4 +28,10 @@ export class AlbumListComponent implements OnInit {
     this.http.delete(`http://localhost:3000/album/${album._id}`).subscribe();
     this.albumList.splice(i, 1);
   }
+
+  updateAlbum() {
+      
+    }
 }
+    
+  
